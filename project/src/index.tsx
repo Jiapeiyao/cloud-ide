105e7d11d97ad781866ca3a6b0c0dd0c6bd43f3b
Jiapeiyao <charlesyjp@qq.com>
2020-06-01 04:39:31 +0800

    add a project

---
+++ project/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from '@hot-loader/react-dom';
+import { hot } from 'react-hot-loader/root';
+import Hello from './Hello';
+import './themes/default/index.less';
+
+ReactDOM.render(
+  React.createElement(hot(Hello)),
+  document.getElementById('app'),
+);
